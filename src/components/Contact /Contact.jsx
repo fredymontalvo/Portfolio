9a7f1aa8a82a40dd1d539bdbcc34ef1100e84036
fredymontalvo <fredualdo@gmail.com>
2024-04-22 16:44:13 +0200

    a & img called with her class

--- src/components/Contact /Contact.jsx
+++ src/components/Contact /Contact.jsx
@@ -2,27 +2,24 @@ import styles from "./contact.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
- const handleScrollTop = () => {
+  const handleScrollTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
- }
+  };
 
   return (
     <footer className={styles.container} id="contact">
       <div className={styles.text}>
         <h2 className={styles.title}>Contact</h2>
-        <p>
-          Feel free to contact me if you have any questions.
-        </p>
+        <p>Feel free to contact me if you have any questions.</p>
       </div>
-      <ul  className={styles.links}>
-        <li  className={styles.link}>
+      <ul className={styles.links}>
+        <li className={styles.link}>
           <img
             src={getImageUrl("contact/envelope-regular.svg")}
             alt="emailIcon"
-    
           />
           <a href="mailto:fredualdo@gmail.com">fredualdo@gmail.com</a>
         </li>
@@ -34,7 +31,7 @@ export const Contact = () => {
           <a href="https://www.linkedin.com/in/fredy-montalvo-ruiz-6b524323b/">
             linkedin.com/fredy-montalvo
           </a>
-        </li >
+        </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/github-142-svgrepo-com.svg")}
